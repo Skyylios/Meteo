7802f4d8181791d695e310048f2a465bc666a004
Firmin Gomez <1424725@cstj.qc.ca>
2025-03-14 11:59:29 -0400

    API Météo

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+  /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    
+    extend: {
+      colors: {
+        color_1: "red",
+        color_2: "blue",
+      },
+    },
+  },
+  plugins: [],
+}
